bf24cb7212c54f9d748c0dd67682c0e5f6b5ce01
sergiomata <32809578+sergiomata@users.noreply.github.com>
2021-05-04 18:23:49 -0500

    refactor: deployDev version 0.1.1

--- handler.js
+++ handler.js
@@ -7,7 +7,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: process.env.ENV
   };
-
+  // test to launch workflow
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
