a7d227e4860e5bdb611aab424594921788b5a484
sergiomata <32809578+sergiomata@users.noreply.github.com>
2021-05-04 21:19:41 -0500

    feat: dotEnv 0.2.0

--- __tests__/template.test.js
+++ __tests__/template.test.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-undef */
 'use strict'
-const dotenv = require('dotenv').config({ path: './.env' });
+const dotenv = require('dotenv').config();
 const { hello } = require('../handler.js')
 
 describe('tests', () => {
   beforeEach(() => { })
-  console.log("process.env.NODE_ENV: ", process.env.NODE_ENV);
   it('integration test error basic auth', async () => {
 
     expect(await hello()).toEqual({
